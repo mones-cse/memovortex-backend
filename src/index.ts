fcf5759ac5c925955503051c6e15393dfdd07edd
mones <mones.cse@gmail.com>
2024-06-15 22:46:15 +0600

    test husky

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express()
 const port = process.env.PORT
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server!!!')
+    res.send('Express + TypeScript Server')
 })
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`)
